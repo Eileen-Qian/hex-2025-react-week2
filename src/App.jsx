36b204312f1db6c2f55512e3e62fdbfa45a5f20a
Eileen-Qian <yiqianc.14@gmail.com>
2026-01-09 17:25:10 +0800

    feat: Implement bootstrap and axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import "./assets/scss/all.scss";
+
+import axios from "axios";
+const API_BASE = import.meta.env.VITE_API_BASE;
+const API_PATH = import.meta.env.VITE_API_PATH;
+console.log(axios);
+console.log(API_BASE, API_PATH);
+
+
 function App() {
   return (
     <>
